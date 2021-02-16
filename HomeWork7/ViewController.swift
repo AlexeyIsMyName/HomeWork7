e650fa5569d8cb40614b75fc4b0caf70baaeb47b
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-02-17 00:26:33 +0300

    Initial Commit

---
+++ HomeWork7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork7
+//
+//  Created by ALEKSEY SUSLOV on 17.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
