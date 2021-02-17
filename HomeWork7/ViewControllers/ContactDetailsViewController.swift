a4d85e60ab7a0da06225704aaa9fad392c82a190
Aleksei <suslov.aleksey.oo7@gmail.com>
2021-02-17 04:32:26 +0300

    first part of homework is done

---
+++ HomeWork7/ViewControllers/ContactDetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ContactDetailsViewController.swift
+//  HomeWork7
+//
+//  Created by ALEKSEY SUSLOV on 17.02.2021.
+//
+
+import UIKit
+
+class ContactDetailsViewController: UIViewController {
+
+    @IBOutlet var phoneNumberLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var person: Person!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = person.fullName
+        phoneNumberLabel.text = person.phoneNumber
+        emailLabel.text = person.email
+    }
+}
--- HomeWork7/ViewControllers/ContactsListTableViewController.swift
+++ HomeWork7/ViewControllers/ContactsListTableViewController.swift
@@ -28,15 +28,11 @@ class ContactsListTableViewController: UITableViewController {
 
         return cell
     }
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        guard let contactDetailsVC = segue.destination as? ContactDetailsViewController else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        contactDetailsVC.person = persons[indexPath.row]
     }
-    */
-
 }
